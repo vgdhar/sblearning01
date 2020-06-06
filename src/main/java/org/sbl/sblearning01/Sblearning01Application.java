3cc76cbd6b307614103105e01968c528d85d1798
vgdhar <gangadhar.v@dxc.com>
2020-06-06 16:02:26 +0530

    commiting the application

---
+++ src/main/java/org/sbl/sblearning01/Sblearning01Application.java
@@ -0,0 +1,14 @@
+package org.sbl.sblearning01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sblearning01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sblearning01Application.class, args);
+		System.out.println("Hello Spring Boot app");
+	}
+
+}
